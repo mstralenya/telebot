d87fb941780958a07eca2ba26169c46e7a415830
Mikhail Stralenia <mstralenya@gmail.com>
2024-12-24 18:12:42 +0300

    Added processing of tiktok links with vm.tiktok.com domain

--- Program.fs
+++ Program.fs
@@ -1,8 +1,12 @@
-﻿open System.IO
+﻿module Program
+
+open System.IO;
 open System.Text.RegularExpressions
 open Funogram.Api
 open Funogram.Telegram
 open Funogram.Telegram.Bot
+open Funogram.Telegram.Types
+open Telebot.TikTok
 
 [<Literal>]
 let replacementsFile = "replacements"
@@ -11,14 +15,28 @@ let readReplacements (filePath: string) =
     File.ReadAllLines filePath
     |> Array.map (fun line -> 
         let parts = line.Split('=')
-        parts.[0], parts.[1])
+        parts[0], parts[1])
     |> Map.ofArray
 
 let replacements: Map<string, string> = readReplacements replacementsFile
 
+let getTikTokLinkIds (input: string option) =
+    match input with
+    | Some text ->
+        let matches = Regex.Matches(text, "http(s)?://(www\.)?(vm.)tiktok.com/(.*)")
+        if matches.Count > 0 then
+            let matched = matches
+                          |> Seq.cast<Match>
+                          |> Seq.toList
+                          |> List.map (_.Value)
+            matched
+        else
+            List.Empty
+    | None -> List.Empty
+
 let applyReplacements (input: string option) =
     match input with
-    | Some (text) ->
+    | Some text ->
         replacements
         |> Map.fold (fun acc key value ->
             let matches = Regex.Matches(text, key)
@@ -30,13 +48,26 @@ let applyReplacements (input: string option) =
                 matched @ acc
             else
                 acc) []
-        |> List.rev
     | None -> List.Empty
 
 let updateArrived (ctx: UpdateContext) =
   match ctx.Update.Message with
   | Some { MessageId = messageId; Chat = chat; Text = messageText } ->
     let replacementList = applyReplacements messageText
+    let tikTokLinks = getTikTokLinkIds messageText
+    
+    tikTokLinks |> List.iter (fun link ->
+            let video = processTikTokVideo (Some link)
+            match video with
+            | Some loadedVideoFile ->
+                let file = InputFile.File(loadedVideoFile, new FileStream(loadedVideoFile, FileMode.Open, FileAccess.Read))
+                Api.sendVideo chat.Id file ""
+                |> api ctx.Config
+                |> Async.Ignore
+                |> Async.RunSynchronously
+                deleteVideo loadedVideoFile
+            | None -> ()
+        )
     replacementList |> List.iter (fun link ->
         Api.sendMessageReply chat.Id link messageId
         |> api ctx.Config
---
+++ TikTok.fs
@@ -0,0 +1,77 @@
+﻿module Telebot.TikTok
+
+open System.IO
+open System.Net.Http
+open System.Text.RegularExpressions
+open Newtonsoft.Json.Linq
+
+let private fetchTikTokFeedAsync videoId =
+    async {
+        let url = $"https://api22-normal-c-alisg.tiktokv.com/aweme/v1/feed/?aweme_id=%s{videoId}&iid=7318518857994389254&device_id=7318517321748022790&channel=googleplay&app_name=musical_ly&version_code=300904&device_platform=android&device_type=ASUS_Z01QD&version=9"
+        use client = new HttpClient()
+        let! response = client.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode() |> ignore
+        let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+        return content
+    }
+
+let private getVideoUrl (json: string) =
+    try
+        let doc = JObject.Parse json
+        let token = doc.SelectToken("aweme_list[0].video.play_addr.url_list[0]")
+        if token <> null then
+            Some (token.ToString())
+        else
+            None
+    with
+    | _ -> None
+
+let private downloadVideoAsync (url: string) (filePath: string) =
+    async {
+        use client = new HttpClient()
+        let! response = client.GetAsync(url) |> Async.AwaitTask
+        response.EnsureSuccessStatusCode() |> ignore
+        let! content = response.Content.ReadAsByteArrayAsync() |> Async.AwaitTask
+        File.WriteAllBytes(filePath, content)
+    }
+
+let private fetchWithHeadersAsync (url: string option) =
+    async {
+        match url with
+        | Some link -> 
+            use client = new HttpClient()
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
+            let! response = client.GetAsync(link: string) |> Async.AwaitTask
+            response.EnsureSuccessStatusCode() |> ignore
+            let! content = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+            let m = Regex.Match(content, "item_id=(\d+)")
+            if m.Success then
+                return Some m.Groups[1].Value
+            else
+                return None
+        | None -> return None
+    }
+    
+let processTikTokVideo (url: string option) : option<string> =
+    let videoId = fetchWithHeadersAsync(url) |> Async.RunSynchronously
+    match videoId with
+    | Some id ->
+        let result = fetchTikTokFeedAsync id |> Async.RunSynchronously
+        match getVideoUrl result with
+        | Some url ->
+            printfn $"Video URL: %s{url}"
+            let filePath = $"{id}.mp4"
+            downloadVideoAsync url filePath |> Async.RunSynchronously
+            printfn $"Video downloaded to %s{filePath}"
+            Some filePath
+        | None ->
+            printfn "Video URL not found"
+            None
+    | None ->
+        printfn "Video ID not found"
+        None
+    
+let deleteVideo (id: string) =
+    let filePath = $"{id}.mp4"
+    if File.Exists(filePath) then
+        File.Delete(filePath)
