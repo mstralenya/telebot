f84cbe8ef66a2bbee1fd77886aa7499a8609a76a
Mikhail Stralenia <mstralenya@gmail.com>
2025-05-14 16:13:03 +0200

    Updated packages, make youtube limited only by size

--- Youtube.fs
+++ Youtube.fs
@@ -43,51 +43,46 @@ let getYoutubeReply (url: string) =
     // Get video metadata
     let! video = youtube.Videos.GetAsync url |> toTask |> Async.AwaitTask
 
-    // Check if the video is longer than 5 minutes
-    if video.Duration.Value > TimeSpan.FromMinutes 5.0 then
-        Log.Information "The video is longer than 5 minutes. Download aborted."
-        return None
-    else
-        let fileName = $"yt_{video.Id}_{Guid.NewGuid()}.mp4"
-        Log.Information $"Downloading video: %s{video.Title}"
+    let fileName = $"yt_{video.Id}_{Guid.NewGuid()}.mp4"
+    Log.Information $"Downloading video: %s{video.Title}"
 
-        // Get available streams for the video
-        let! streamManifest = youtube.Videos.Streams.GetManifestAsync url |> toTask |> Async.AwaitTask
+    // Get available streams for the video
+    let! streamManifest = youtube.Videos.Streams.GetManifestAsync url |> toTask |> Async.AwaitTask
 
-        Log.Information $"Video Stream options: %A{streamManifest.GetVideoOnlyStreams() |> Seq.toList |> List.map (fun x -> x.Container, x.Bitrate, x.Size, x.VideoResolution, x.VideoCodec)}"
-        Log.Information $"Audio Stream options: %A{streamManifest.GetAudioOnlyStreams() |> Seq.toList |> List.map (fun x -> x.Container, x.Bitrate, x.Size, x.AudioCodec)}"
+    Log.Information $"Video Stream options: %A{streamManifest.GetVideoOnlyStreams() |> Seq.toList |> List.map (fun x -> x.Container, x.Bitrate, x.Size, x.VideoResolution, x.VideoCodec)}"
+    Log.Information $"Audio Stream options: %A{streamManifest.GetAudioOnlyStreams() |> Seq.toList |> List.map (fun x -> x.Container, x.Bitrate, x.Size, x.AudioCodec)}"
+    
+    // Get the best video stream (e.g., highest quality)
+    let videoStream = getVideoStream streamManifest
+    let audioStream = streamManifest.GetAudioOnlyStreams()
+                      |> Seq.filter (fun c -> c.Size.MegaBytes < 2)
+                      |> Seq.tryMaxBy _.Bitrate.KiloBitsPerSecond
+
+    match videoStream, audioStream with
+    | Some videoS, Some audioS ->
+        // Download the video
+        Log.Information $"Downloading stream: %s{videoS.Url}"
+
+        let streams : IStreamInfo list = [videoS; audioS]
+        let conversionRequest = ConversionRequestBuilder(fileName)
+                                    .SetPreset(ConversionPreset.Fast)
+                                    .SetContainer(Container.Mp4)
+                                    .Build()
+        youtube.Videos.DownloadAsync(streams, conversionRequest)
+            |> _.AsTask() // Convert ValueTask to Task
+            |> Async.AwaitTask // Await the Task
+            |> Async.RunSynchronously
+
+        Log.Information $"Video downloaded successfully: %s{fileName}, resolution {videoS.VideoResolution}, fileSize {videoS.Size}"
+
+        let title = Some video.Title
+        let reply = createVideoFileWithCaption fileName title
         
-        // Get the best video stream (e.g., highest quality)
-        let videoStream = getVideoStream streamManifest
-        let audioStream = streamManifest.GetAudioOnlyStreams()
-                          |> Seq.filter (fun c -> c.Size.MegaBytes < 2)
-                          |> Seq.tryMaxBy _.Bitrate.KiloBitsPerSecond
-
-        match videoStream, audioStream with
-        | Some videoS, Some audioS ->
-            // Download the video
-            Log.Information $"Downloading stream: %s{videoS.Url}"
-
-            let streams : IStreamInfo list = [videoS; audioS]
-            let conversionRequest = ConversionRequestBuilder(fileName)
-                                        .SetPreset(ConversionPreset.Fast)
-                                        .SetContainer(Container.Mp4)
-                                        .Build()
-            youtube.Videos.DownloadAsync(streams, conversionRequest)
-                |> _.AsTask() // Convert ValueTask to Task
-                |> Async.AwaitTask // Await the Task
-                |> Async.RunSynchronously
-
-            Log.Information $"Video downloaded successfully: %s{fileName}, resolution {videoS.VideoResolution}, fileSize {videoS.Size}"
-
-            let title = Some video.Title
-            let reply = createVideoFileWithCaption fileName title
-            
-            return Some reply
-        | _ ->
-            Log.Warning "No suitable streams found for the video. Download aborted."
-            let message = createMessage "Cannot download video due size limits"
-            return Some message
+        return Some reply
+    | _ ->
+        Log.Warning "No suitable streams found for the video. Download aborted."
+        let message = createMessage "Cannot download video due size limits - max size is 50 MiB"
+        return Some message
         
         
     } |> Async.RunSynchronously
